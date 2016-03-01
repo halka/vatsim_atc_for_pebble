ead975f7fdd51849d70d33e266bf3ef508aba4f0
a_halka <halka.rjch@gmail.com>
2016-03-01 13:34:43 +0900

    implement station details
    name. VID, NOTAM

--- src/app.js
+++ src/app.js
@@ -3,20 +3,35 @@ var ajax = require('ajax');
 var URL = 'http://exp.rw12.net/vatsim/gen_json.php';
 
 var getStationList = function(data){
-  var items = [];
+  var menuItems = [];
   for(var i = 0; i < data.length; i++){
     if(data[i][3].toString() == 'ATC'){
       var station_name = data[i][0].toString();
       var freq = data[i][4].toString();
-      items.push({
+      menuItems.push({
         title:station_name,
         subtitle:freq
       });
     }
   }
-  return items;
+  return menuItems;
 };
 
+var getStationDetails = function(data){
+  var details = [];
+  for(var i = 0; i < data.length; i++){
+    if(data[i][3].toString() == 'ATC'){
+      var station_name = data[i][0].toString();
+      var freq = data[i][4].toString();
+      details.push({
+        title:station_name,
+        subtitle:freq,
+        body:data[i][2].toString()+'('+data[i][1].toString()+')\n'+data[i][35]
+      });
+    }
+  }
+  return details;
+};
 ajax(
   {
     url: URL,
@@ -31,16 +46,15 @@ ajax(
     }]  
   });
   resultsMenu.show();
+    
   resultsMenu.on('select', function(e){
-    if(data[e.itemIndex][3].toString() == 'ATC'){
-    var controller = data[e.itemIndex][0];
+    //var controller = data[e.itemIndex][0];
     var detailCard = new UI.Card({
-    title:controller,
-    subtitle:'sub',
-    body:'body'
+    title:getStationDetails(data)[e.itemIndex].title,
+    subtitle:getStationDetails(data)[e.itemIndex].subtitle,
+    body:getStationDetails(data)[e.itemIndex].body
     });
       detailCard.show();
-    }
   });
   },
   function(error) {
